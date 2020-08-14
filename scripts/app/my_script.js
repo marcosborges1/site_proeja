1b6861182747488eaeef9ed7a2892647fc507496
marcosborges1 <borges.marcos1@gmail.com>
2020-08-14 14:17:28 -0300

    Versão melhorada em relação a quebra da tabela

--- scripts/app/my_script.js
+++ scripts/app/my_script.js
@@ -1,21 +1,3 @@
-/*
- |--------------------------------------------------------------------------
- | Shards Dashboards: Blog Overview Template
- |--------------------------------------------------------------------------
- */
-
-
-// 'use strict';
-
-// (function ($) {
-//   $(document).ready(function () {
-
-//     //Configuration Marcos (File MyUtils.js)
-//     MyUtils.includeHTML()
-//     //End Configuration Marcos
-//   });
-// })(jQuery);
-
 const getInformacoesProejaPorCodigo = (codigo) => {
 	$.getJSON(`https://spreadsheets.google.com/feeds/cells/1nvHPe949X6WV_toO7MwNhikiPAREo5Sn4uBzN-jru58/${codigo}/public/full?alt=json`, (data)=> {
 
@@ -30,15 +12,25 @@ const getInformacoesProejaPorCodigo = (codigo) => {
 		})
 
 		$(`#loading_${codigo}`).hide()
-		$(`#proeja_turma_${codigo}`).html(x)    
+		$(`#proeja_turma_${codigo}`).html(x) 
+		showHorario(codigo)
+		
 	})
 }
 function tupla(r,i) {
 
-	if(i%5==2) {
-	return `<td class="border-0 d-none d-lg-block d-sm-none d-md-block">${r.content.$t}</td>`
+	if(i%5==2 || i%5==4) {
+		return `<td class="border-0 d-none d-lg-block d-sm-none d-md-block">${r.content.$t}</td>`
 	}
-	return `
-	<td>${r.content.$t}</td>
-	`
+	return `<td>${r.content.$t}</td>`
+}
+
+function showHorario(codigo) {
+	$(`#proeja_turma_${codigo}`).find("td").map((i,x)=>{
+		if(i%5==2) {
+			let object = $(`#proeja_turma_${codigo}`).find("td")[i]
+			let nextObject = $($(`#proeja_turma_${codigo}`).find("td")[i+1]).html()
+			$(object).html($(object).html()+ `<u class='border-0 d-block d-lg-none d-sm-none d-md-none'>${nextObject}</u>`)
+		}
+	})   
 }
